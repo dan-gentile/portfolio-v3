5217ebdc219c23abd946c469b78419df47cc0dd0
Dan Gentile <dangentile@ymail.com>
2020-12-01 15:03:17 -0800

    added Portfolio to the page and The Lumineers to the page

--- src/Projects/Lumineers.jsx
+++ src/Projects/Lumineers.jsx
@@ -3,6 +3,7 @@ import React, { useRef } from "react";
 import BoxMesh from "../components/BoxMesh/BoxMesh";
 import Modal from "../components/Modal/Modal";
 import TheLumineers from "../assets/images/the-lumineers.png";
+import TheLumineersShow from "../assets/images/theLumineersShow.jpg";
 
 const Lumineers = () => {
   const theLumineersRef = useRef();
@@ -23,7 +24,40 @@ const Lumineers = () => {
       />
       <Html>
         <Modal ref={theLumineersRef} color="#959595">
-          <p>The Lumineers</p>
+          <div className="touring">
+            <h3>The Lumineers</h3>
+            <img
+              src={TheLumineersShow}
+              alt="The Killers Live"
+              style={{ width: "300px", borderRadius: "20px" }}
+            />
+            <h4>Disguise d3 programmer and operator</h4>
+            <h5>2016-2017 | 2019-2020</h5>
+            <p>
+              Responsibilities included setting up the server daily, updating
+              pixel mapping, projection mapping and cueing as necessary.
+              Traveled with the show for the entire record cycle including
+              headlining and festival shows across the globe. In addition to
+              content playback the server handled various live inputs. Sometimes
+              these feeds were clean straight to the screens. Other times the
+              feeds were fed through various Notch realtime fxs before heading
+              to the screens. During the show the server control was split
+              between the lighting console using SockPuppet and MIDI control.
+            </p>
+            <h5>Links</h5>
+            <ul>
+              <li>
+                <a
+                  className="light"
+                  href="https://plsn.com/articles/production-profile/the-lumineers-iii-tour/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  PLSN Magazine Article
+                </a>
+              </li>
+            </ul>
+          </div>
         </Modal>
       </Html>
     </>
--- src/Projects/Portfolio.jsx
+++ src/Projects/Portfolio.jsx
@@ -3,6 +3,9 @@ import React, { useRef } from "react";
 import BoxMesh from "../components/BoxMesh/BoxMesh";
 import Modal from "../components/Modal/Modal";
 import PortfolioPic from "../assets/images/portfolio.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faDesktop } from "@fortawesome/free-solid-svg-icons";
+import { faGithubAlt } from "@fortawesome/free-brands-svg-icons";
 
 const Portfolio = () => {
   const portfolioRef = useRef();
@@ -23,7 +26,65 @@ const Portfolio = () => {
       />
       <Html>
         <Modal ref={portfolioRef} color="#959595">
-          <p>Portfolio</p>
+          <div className="projects">
+            <h3>Hey! You're Here!</h3>
+            <h4>
+              <FontAwesomeIcon
+                icon={faCode}
+                size="1x"
+                style={{ marginRight: "4px" }}
+              />
+              React | SCSS | Three.js | React-Spring | Framer-Motion
+            </h4>
+            <ul>
+              <li>
+                <a
+                  className="light"
+                  href="https://github.com/dan-gentile/portfolio-v3"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faGithubAlt}
+                    size="1x"
+                    style={{ marginRight: "4px" }}
+                  />
+                  GitHub |
+                </a>
+              </li>
+              <li>
+                <a
+                  className="light"
+                  href="https://github.com/dan-gentile/portfolio-v3"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FontAwesomeIcon
+                    icon={faDesktop}
+                    size="1x"
+                    style={{ marginRight: "4px", marginLeft: "4px" }}
+                  />
+                  Website
+                </a>
+              </li>
+            </ul>
+            <p>
+              A portfolio page should best reflect the creator. For me the last
+              10 years of my life have revolved around live events. <br />
+              Throughout that time responsibilities have grown and evolved. The
+              last few years it has been mostly focused on video whether that be
+              creation, programming systems or playback. <br />
+              Recent projects have involved projection mapping or pixel mapping
+              to complex and simple shapes. During the process 3D models are
+              crucial so I thought it would be fun to include some <br />
+              here in the site.
+            </p>
+            <p>
+              Whether its the past, present or future. Bringing ideas to reality
+              are the creative challenges I strive for, and this sure was a
+              challenge.
+            </p>
+          </div>
         </Modal>
       </Html>
     </>
